92e9efb0e0f3060757447d88a50cdd47a3046654
BinaryConvertz <ryangregoire286@gmail.com>
2026-03-03 12:11:19 -0500

    added mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
+import {Connect} from "./DB";
+
 function OPENUrl(url: string) {
 
     return window.open(url);
 }
 
-function URLS() {
+
+
+async function URLS() {
 
     OPENUrl("https://github.com/ryangregoire286-star");
+    await Connect("https://github.com/ryangregoire286-star");
 }
 
 const button = document.getElementById("button");
