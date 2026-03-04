4d8ca6b79f57e29d091de6fb1aee4b0a15d33486
BinaryConvertz <ryangregoire286@gmail.com>
2026-03-03 19:38:43 -0500

    commented out some of the mongodb code

---
+++ src/Imports.ts
@@ -0,0 +1,12 @@
+export const button =
+    document.getElementById("button") as HTMLButtonElement;
+export const buttonLight =
+    document.getElementById("buttonLight") as HTMLButtonElement
+
+export function OPENUrl(url: string) {
+
+    return window.open(url);
+}
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
-import {Connect} from "./DB";
-
-function OPENUrl(url: string) {
-
-    return window.open(url);
-}
-
-
+// import {Connect} from "./DB";
+import {button, buttonLight, OPENUrl} from "./Imports";
 
 async function URLS() {
 
     OPENUrl("https://github.com/ryangregoire286-star");
-    await Connect("https://github.com/ryangregoire286-star");
+    // await Connect("https://github.com/ryangregoire286-star");
+    button.textContent = "Clicked";
 }
 
-const button = document.getElementById("button");
-button?.addEventListener("click", URLS)
+function SetLight() {
+    return "#ccc";
+}
+
+button.addEventListener("click", URLS)
+buttonLight.addEventListener("click", () => {
+
+    document.body.style.backgroundColor = SetLight();
+})
