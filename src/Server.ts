9c8fa836cd73496f1446e25c9f80a004e70969d1
Ryangregoire286-star <ryangregoire286@gmail.com>
2026-03-07 18:18:32 -0500

    changed style and formatting of code readability added font awesome icons for other pages and import CSS Styles File

--- src/Server.ts
+++ src/Server.ts
@@ -1,16 +1,15 @@
 import express from 'express';
+import * as path from "node:path";
 
 const SetupServer = () => {
 
     const app = express();
-    const PORT = process.env.PORT || 5000 ;
+    const PORT = process.env.PORT || 5000;
+    app.use(express.static("./public/Pages"))
 
-    app.use(express.static("./public"))
 
-
-    app.listen(PORT, () => {
+    app.listen(5000, () => {
         console.log("Listening " + `http://localhost:${PORT}`);
-    })
+    });
 }
-
 SetupServer();
