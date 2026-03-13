f576df6e498bc7c5a40ad29008c4dc20c6193363
Ryangregoire286-star <ryangregoire286@gmail.com>
2026-03-12 22:15:17 -0400

    added pub template engine like ejs but pug template for parsing html file code

--- src/Server.ts
+++ src/Server.ts
@@ -3,19 +3,23 @@ import express from "express";
 import * as path from "path";
 
 const messageFunc = (isRunning: boolean) => {
-  return isRunning == true ? "Running" : "Not Running";
+  return isRunning ? "Running" : "Not Running";
 };
 
 enum PickedRoute {
-  HOME,
-  ABOUT,
+  // ABOUT,
   OTHER,
-  CONTACT,
+  TODOS
 }
 
 const URLCreate = (url: PickedRoute) => {
   if (url == PickedRoute.OTHER) {
     return "/other";
+  }
+
+  if (url == PickedRoute.TODOS) {
+
+    return "/Todos";
   }
 
   return url;
@@ -30,21 +34,30 @@ const SetupServer = () => {
   const port = client.isRunning();
 
   const URLsCreate = URLCreate(PickedRoute.OTHER);
+  // const AboutPageURL = URLCreate(PickedRoute.ABOUT);
+  const todosRoute = URLCreate(PickedRoute.TODOS);
 
-  app.get(URLsCreate.toString(), (req, res) => {
+  app.get(URLsCreate.toString(), (_req, res) => {
     res.sendFile(
       path.join(__dirname, "/public/Pages/MathRoute/MathRoute.html"),
     );
   });
 
   const message = client.isRunningServer();
-  if (message === true) {
+  if (message) {
     const messageServer = messageFunc(message);
     app.listen(port, () => {
       console.log("Listening " + `http://localhost:${port}`);
       console.log("Server is Currently: " + messageServer);
     });
   }
+
+  app.get(todosRoute.toString(), (_req, res) => {
+    res.sendFile(
+        path.join(__dirname, "/public/Pages/TodosList/TodoList.html"),
+    );
+  });
 };
+
 
 SetupServer();
